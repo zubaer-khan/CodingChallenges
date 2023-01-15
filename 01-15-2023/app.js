ea2ff6d67d6eb56e17985fe5d1474af94768cf67
zubaer-khan <24850351+zubaer-khan@users.noreply.github.com>
2023-01-15 09:25:39 -0500

    Add coding challenge for 01-15-2023

---
+++ 01-15-2023/app.js
@@ -0,0 +1,15 @@
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+function fakeBin(x){
+    let y = ''
+    for (let i = 0, n = x.length; i < n; i++) {
+      if (x[i] < 5) {
+        y += '0'
+      } else {
+        y += '1'
+      }
+    }
+      return y;
+  }
