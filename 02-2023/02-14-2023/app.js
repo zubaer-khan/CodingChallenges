d73a1200de9d0ffaadc5f8c2f3a6f5ce98380d36
zubaer-khan <24850351+zubaer-khan@users.noreply.github.com>
2023-02-14 19:38:43 -0500

    Add coding challenge for 02-14-2023

---
+++ 02-2023/02-14-2023/app.js
@@ -0,0 +1,24 @@
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+function findOdd(A) {
+    A.sort((a, b) => a - b)
+    if (A.length === 1) {
+           return A[0]
+      }
+    for (i = 0; i < A.length - 1; i += 2){
+        if (A[i] !== A[i+1]){
+           return A[i]
+        }
+    }
+        return A[A.length - 1]
+  }
+  
