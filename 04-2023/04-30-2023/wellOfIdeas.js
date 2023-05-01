e2f11bb146c1819e51670236e53f4cc4118d46b2
zubaer-khan <24850351+zubaer-khan@users.noreply.github.com>
2023-04-30 22:22:20 -0400

    added coding challenge Well of Ideas

---
+++ 04-2023/04-30-2023/wellOfIdeas.js
@@ -0,0 +1,8 @@
+// For every good kata idea there seem to be quite a few bad ones!
+
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+
+
+const well = (x) => !x.includes('good') ? 'Fail!' :
+                    x.filter(x => x === 'good').length > 2 ? 'I smell a series!': 'Publish!'
