cc96cee7abc574fa2f6d1ef103026aeeaa8beda2
zubaer-khan <24850351+zubaer-khan@users.noreply.github.com>
2023-05-05 22:37:57 -0400

    words with more than 5 letters are reversed

---
+++ stopGninnipsMySdrow.js
@@ -0,0 +1,20 @@
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+// Examples:
+
+// spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+// spinWords( "This is a test") => returns "This is a test" 
+// spinWords( "This is another test" )=> returns "This is rehtona test"
+
+function spinWords(string){
+    let arr = string.split(' ')
+    let newArr = []
+    arr.forEach((x) => {
+      if (x.length > 4){
+        newArr.push(x.split('').reverse().join(''))
+      }else{
+        newArr.push(x)
+      }
+    })
+    return newArr.join(' ')
+  }
