e9c94392744d99e42da4d35499b6e1f2d954cd0c
zubaer-khan <24850351+zubaer-khan@users.noreply.github.com>
2023-05-29 18:47:34 -0400

    count and return the number of consonants in the string

---
+++ consonantCount.js
@@ -0,0 +1,21 @@
+// Complete the function that takes a string of English-language text and returns the number of consonants in the string.
+
+// Consonants are all letters used to write English excluding the vowels a, e, i, o, u.
+
+
+
+function consonantCount(str) {
+    // take in a string of text
+    // return the number of consonants in the string
+    // example: consonantCount('hello', 3)
+    
+    // trim() string to remove white spaces
+    // split into an array
+    // filter array by !includes 'aeiouAEIOU'
+    // return the length of the new array
+    
+    return str.trim()
+              .split('')
+              .filter((x) => 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'.includes(x))
+              .length
+  }
