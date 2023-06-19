5566404d302b8535232ab6de7f24594482c32f97
zubaer-khan <24850351+zubaer-khan@users.noreply.github.com>
2023-06-18 23:42:13 -0400

    set lamps to display os for all xs up until the drone position

---
+++ droneFlyBy.js
@@ -0,0 +1,16 @@
+// The other day I saw an amazing video where a guy hacked some wifi controlled lightbulbs by flying a drone past them. Brilliant.
+
+// In this kata we will recreate that stunt... sort of.
+
+// You will be given two strings: lamps and drone. lamps represents a row of lamps, currently off, each represented by x. When these lamps are on, they should be represented by o.
+
+// The drone string represents the position of the drone T (any better suggestion for character??) and its flight path up until this point =. The drone always flies left to right, and always begins at the start of the row of lamps. Anywhere the drone has flown, including its current position, will result in the lamp at that position switching on.
+
+// Return the resulting lamps string. See example tests for more clarity.
+
+function flyBy(lamps, drone){
+    let dist = drone.indexOf('T') + 1
+    let secondHalf = lamps.slice(dist)
+    let firstHalf = lamps.slice(0, dist).split('').map((x) => 'o').join('')
+    return `${firstHalf}${secondHalf}`
+  }
