70047f3e25b7c9fc763362a8df6c2e2f29943d37
zubaer-khan <24850351+zubaer-khan@users.noreply.github.com>
2023-06-26 21:51:18 -0400

    remove whitepaces between letters of a word

---
+++ ghostbusters.js
@@ -0,0 +1,20 @@
+// Oh no! Ghosts have reportedly swarmed the city. It's your job to get rid of them and save the day!
+
+// In this kata, strings represent buildings while whitespaces within those strings represent ghosts.
+
+// So what are you waiting for? Return the building(string) without any ghosts(whitespaces)!
+
+// Example:
+
+// ghostBusters("Sky scra per");
+// Should return:
+
+// "Skyscraper"
+// If the building contains no ghosts, return the string:
+
+// "You just wanted my autograph didn't you?"
+
+function ghostBusters(building) {
+  let arr = building.split(' ')
+  return arr.length !== 1 ? arr.map((x) => x.trim()).join('') : "You just wanted my autograph didn't you?"
+}
