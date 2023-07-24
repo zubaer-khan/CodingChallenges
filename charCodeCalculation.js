1a5aca4e68aa6bb327e194e287315239ff877e26
zubaer-khan <24850351+zubaer-khan@users.noreply.github.com>
2023-07-23 21:39:48 -0400

    replace all chars with the matching charcode, then replace all 7s with 1s and add all the digits and subtract from the original number with all digits added...phew

---
+++ charCodeCalculation.js
@@ -0,0 +1,24 @@
+// Given a string, turn each character into its ASCII character code and join them together to create a number - let's call this number total1:
+
+// 'ABC' --> 'A' = 65, 'B' = 66, 'C' = 67 --> 656667
+// Then replace any incidence of the number 7 with the number 1, and call this number 'total2':
+
+// total1 = 656667
+//               ^
+// total2 = 656661
+//               ^
+// Then return the difference between the sum of the digits in total1 and total2:
+
+//   (6 + 5 + 6 + 6 + 6 + 7)
+// - (6 + 5 + 6 + 6 + 6 + 1)
+// -------------------------
+//                        6
+
+function calc(x){
+    let total1 = x.split('').map(x => x.charCodeAt(0)).join('')
+    let total2 = total1.replaceAll('7', '1')
+  
+    let total1Reduced = total1.split('').map(x => +x).reduce((a,b) => a + b, 0)
+    let total2Reduced = total2.split('').map(x => +x).reduce((a,b) => a + b, 0)
+    return total1Reduced - total2Reduced
+  }
