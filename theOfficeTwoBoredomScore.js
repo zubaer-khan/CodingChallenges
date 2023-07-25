639a1bdc91999bcd223a464f46fcf1106eed24b5
zubaer-khan <24850351+zubaer-khan@users.noreply.github.com>
2023-07-24 23:25:56 -0400

    loop through an object to determine the score of each individual, then add those scores to determine the overall score and return the required string

---
+++ theOfficeTwoBoredomScore.js
@@ -0,0 +1,65 @@
+// Every now and then people in the office moves teams or departments. Depending what people are doing with their time they can become more or less boring. Time to assess the current team.
+
+// You will be provided with an object(staff) containing the staff names as keys, and the department they work in as values.
+
+// Each department has a different boredom assessment score, as follows:
+
+// accounts = 1
+// finance = 2
+// canteen = 10
+// regulation = 3
+// trading = 6
+// change = 6
+// IS = 8
+// retail = 5
+// cleaning = 4
+// pissing about = 25
+
+// Depending on the cumulative score of the team, return the appropriate sentiment:
+
+// <=80: 'kill me now'
+// < 100 & > 80: 'i can handle this'
+// 100 or over: 'party time!!'
+
+function boredom(staff){
+    let result = []
+    for (let dep in staff){
+      result.push(staff[dep])
+    }
+    let count = 0
+    for(let i = 0; i < result.length; i++){
+      switch (result[i]){
+          case 'accounts':
+            count++;
+            break;
+          case 'finance': 
+            count += 2;
+            break;
+          case 'canteen':
+            count += 10;
+            break;
+          case 'regulation':
+            count += 3;
+            break
+          case 'trading': 
+            count += 6;
+            break;
+          case 'change': 
+            count += 6;
+            break;
+          case 'IS': 
+            count += 8;
+            break;
+          case 'retail': 
+            count += 5;
+            break;
+          case 'cleaning': 
+            count += 4;
+            break;
+          case 'pissing about': 
+            count += 25;
+            break;
+      }
+    }
+    return count >= 100 ? 'party time!!' : count > 80 ? 'i can handle this' : 'kill me now'
+  }
