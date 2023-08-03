928872956435906b3291feabcfd07fbef3181121
zubaer-khan <24850351+zubaer-khan@users.noreply.github.com>
2023-08-02 23:16:47 -0500

    determine if an array is sorted and how and return the answer

---
+++ sortedYesNoHow.js
@@ -0,0 +1,19 @@
+// Complete the method which accepts an array of integers, and returns one of the following:
+
+// "yes, ascending" - if the numbers in the array are sorted in an ascending order
+// "yes, descending" - if the numbers in the array are sorted in a descending order
+// "no" - otherwise
+// You can assume the array will always be valid, and there will always be one correct answer.
+
+function isSortedAndHow(array) {
+    let nums = [...array]
+    console.log(nums)
+    if (array.every((x,i) => x == nums.sort()[i])){
+      return 'yes, ascending' 
+    }else if(array.every((x,i) => x == nums.sort((a,b) => b - a)[i])){
+      return 'yes, descending'
+    }else{
+      return 'no' 
+    }
+  }
+  
